ec186a0b83dfc73716523815e7932f25dee7360d
marvin-j97 <marvin.janke.97@gmail.com>
2024-10-15 16:44:16 +0200

    test: tag idx

--- src/tag_index.rs
+++ src/tag_index.rs
@@ -1,4 +1,4 @@
-use crate::SeriesId;
+use crate::{SeriesId, TagSet};
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use fjall::{CompressionType, PartitionCreateOptions, TxKeyspace, TxPartition, WriteTransaction};
 
@@ -42,6 +42,23 @@ impl TagIndex {
     pub fn index(
         &self,
         tx: &mut WriteTransaction,
+        metric: &str,
+        tags: &TagSet,
+        series_id: SeriesId,
+    ) -> fjall::Result<()> {
+        self.index_term(tx, metric, series_id)?;
+
+        for (key, value) in tags {
+            let term = format!("{metric}#{key}:{value}");
+            self.index_term(tx, &term, series_id)?;
+        }
+
+        Ok(())
+    }
+
+    fn index_term(
+        &self,
+        tx: &mut WriteTransaction,
         term: &str,
         series_id: SeriesId,
     ) -> fjall::Result<()> {
@@ -72,7 +89,8 @@ impl TagIndex {
         Ok(())
     }
 
-    pub fn query(&self, term: &str) -> fjall::Result<Vec<SeriesId>> {
+    // TODO: read_tx
+    pub fn query_eq(&self, term: &str) -> fjall::Result<Vec<SeriesId>> {
         Ok(self
             .partition
             .get(term)?
@@ -89,5 +107,169 @@ impl TagIndex {
                 postings
             })
             .unwrap_or_default())
+    }
+
+    // TODO: read_tx
+    pub fn query_prefix(&self, metric: &str, prefix: &str) -> fjall::Result<Vec<SeriesId>> {
+        let mut ids = vec![];
+
+        for kv in self.partition.inner().prefix(format!("{metric}#{prefix}")) {
+            let (_, v) = kv?;
+
+            let mut reader = &v[..];
+
+            let len = reader.read_u64::<BigEndian>().expect("should deserialize");
+            let mut postings = Vec::with_capacity(len as usize);
+
+            for _ in 0..len {
+                postings.push(reader.read_u64::<BigEndian>().expect("should deserialize"));
+            }
+
+            ids.extend(postings);
+        }
+
+        ids.sort();
+        ids.dedup();
+
+        Ok(ids)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_log::test]
+    fn test_tag_index_prefix() -> crate::Result<()> {
+        let path = tempfile::tempdir()?;
+        let keyspace = fjall::Config::new(&path).open_transactional()?;
+        let tag_index = TagIndex::new(&keyspace)?;
+        let metric = "cpu.total";
+
+        let mut tx = keyspace.write_tx();
+
+        {
+            let tags = crate::tagset!(
+                "service" => "prod-db",
+            );
+            tag_index.index(&mut tx, metric, tags, 0)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "service" => "staging-db",
+            );
+            tag_index.index(&mut tx, metric, tags, 1)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "service" => "test-db",
+            );
+            tag_index.index(&mut tx, metric, tags, 2)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "service" => "prod-ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 3)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "service" => "staging-ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 4)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "service" => "test-ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 5)?;
+        }
+
+        tx.commit()?;
+
+        assert_eq!(vec![0, 3], tag_index.query_prefix(metric, "service:prod-")?);
+
+        Ok(())
+    }
+
+    #[test_log::test]
+    fn test_tag_index_eq() -> crate::Result<()> {
+        let path = tempfile::tempdir()?;
+        let keyspace = fjall::Config::new(&path).open_transactional()?;
+        let tag_index = TagIndex::new(&keyspace)?;
+        let metric = "cpu.total";
+
+        let mut tx = keyspace.write_tx();
+
+        {
+            let tags = crate::tagset!(
+                "env" => "prod",
+                "service" => "db",
+            );
+            tag_index.index(&mut tx, metric, tags, 0)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "dev",
+                "service" => "db",
+            );
+            tag_index.index(&mut tx, metric, tags, 1)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "test",
+                "service" => "db",
+            );
+            tag_index.index(&mut tx, metric, tags, 2)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "staging",
+                "service" => "db",
+            );
+            tag_index.index(&mut tx, metric, tags, 3)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "prod",
+                "service" => "ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 4)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "dev",
+                "service" => "ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 5)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "test",
+                "service" => "ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 6)?;
+        }
+        {
+            let tags = crate::tagset!(
+                "env" => "staging",
+                "service" => "ui",
+            );
+            tag_index.index(&mut tx, metric, tags, 7)?;
+        }
+
+        tx.commit()?;
+
+        assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7], tag_index.query_eq(metric)?);
+        assert_eq!(
+            vec![0, 4],
+            tag_index.query_eq(&format!("{metric}#env:prod"))?
+        );
+        assert_eq!(
+            vec![4, 5, 6, 7],
+            tag_index.query_eq(&format!("{metric}#service:ui"))?
+        );
+
+        Ok(())
     }
 }
