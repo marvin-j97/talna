b49e1c8a77a6598980a130f649ea10d4b5dbe38c
marvin-j97 <marvin.janke.97@gmail.com>
2024-10-18 21:43:54 +0200

    fmt

--- src/time.rs
+++ src/time.rs
@@ -1,7 +1,8 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Returns the current timestamp in nanoseconds.
-#[must_use] pub fn timestamp() -> u128 {
+#[must_use]
+pub fn timestamp() -> u128 {
     let start = SystemTime::now();
     let since_the_epoch = start
         .duration_since(UNIX_EPOCH)
